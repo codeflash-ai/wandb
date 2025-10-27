492034c1768d71c4d8dd183d9711de5ec79c5a51
Jacob Romero <jacob.romero@wandb.com>
2025-10-27 13:45:51 -0700

    rebase

--- core/internal/runhistoryreader/reader.go
+++ core/internal/runhistoryreader/reader.go
@@ -115,10 +115,13 @@ func (h *HistoryReader) GetHistorySteps(
 			float64(maxStep),
 			false,
 		)
+
+		selectAllColumns := len(h.keys) == 0
 		selectedColumns, err := iterator.SelectColumns(
 			iterator.StepKey,
 			h.keys,
 			parquetFile.ParquetReader().MetaData().Schema,
+			selectAllColumns,
 		)
 		if err != nil {
 			return nil, err
